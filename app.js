5b88da326e983bfefeaf3c3b65f291e6a47cd80e
DeniseCristino <cristinodenise@hotmail.com>
2025-03-18 15:23:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
-let amigos = [];
 
-/**
- * Agrega un nuevo amigo al array de amigos.
- * Valida que el nombre no esté vacío antes de agregarlo y actualiza la lista visual.
- */
+let listaDeAmigos = [];
+let nombreAmigo = "";
+
+function cambiarElemento(idElemento,textoElemento) {
+    document.getElementById(idElemento).innerHTML = textoElemento;
+}
+
 function agregarAmigo() {
-  let nombreAmigo = document.getElementById("amigo").value;
-
-  if (nombreAmigo.trim() === "") {
-    alert("Por favor, inserte un nombre");
-  } else {
-    amigos.push(nombreAmigo);
-    document.querySelector("#amigo").value = "";
-    mostrarListaAmigo();
-  }
+    nombreAmigo = document.querySelector("#amigo").value;
+    if(nombreAmigo==""){
+        alert("Debes escribir un nombre para agregar");
+    }else{
+        if(listaDeAmigos.includes(nombreAmigo)){
+            alert("No puedes repetir el nombre de amigo");
+        }else{
+            listaDeAmigos.push(nombreAmigo);
+            document.querySelector("#amigo").value = "";
+        }
+    }
+    console.log(listaDeAmigos);
+    listarAmigos();
+    return;
 }
 
-/**
- * Actualiza la visualización de la lista de amigos en el DOM, creando elementos <li> para cada amigo.
- */
-function mostrarListaAmigo() {
-  let listaAmigos = document.querySelector("#listaAmigos");
-  listaAmigos.innerHTML = "";
-
-  for (let index = 0; index < amigos.length; index++) {
-    const element = amigos[index];
-
-    let listaHTML = document.createElement("li");
-    listaHTML.textContent = element;
-    listaAmigos.appendChild(listaHTML);
-  }
+function listarAmigos() {
+    const ulTag = document.getElementById("listaAmigos");
+    ulTag.innerHTML = "";
+    if(listaDeAmigos.length > 0){
+            for(i = 0;i < listaDeAmigos.length;i++){
+            //generamos un li para cada amigo dentro del ul existente
+            const liTag = document.createElement("li");
+            liTag.innerHTML = listaDeAmigos[i];
+            ulTag.appendChild(liTag);
+        }
+    }
 }
 
-/**
- * Sortea y muestra un amigo de la lista de amigos de manera aleatoria.
- * Verifica que la lista no esté vacía antes de realizar el sorteo.
- */
 function sortearAmigo() {
-  let cantidadAmigos = amigos.length;
-  if (cantidadAmigos === 0) {
-    alert("Por favor, inserte un nombre antes de sortear");
-  } else {
-    let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
-    let resultadoHTML = document.querySelector("#resultado");
-    resultadoHTML.innerHTML = amigos[indiceAmigo];
-  }
+    if(listaDeAmigos.length==0){
+        alert("No hay amigos en la lista para sortear");
+    }else{
+        let indice = Math.floor(Math.random() * listaDeAmigos.length);
+        const ulRes = document.getElementById("resultado");
+        const liRes = document.createElement("li");
+        liRes.innerHTML = listaDeAmigos[indice];
+        ulRes.appendChild(liRes);
+    }
+    return;
 }
